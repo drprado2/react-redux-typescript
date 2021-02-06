2d4785c5c578c313ce86437d72a6e3758e47f6e5
Adriano Oliveira <drprado2@gmail.com>
2021-02-06 14:15:18 -0300

    Adding eslint, editor config and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/global.scss';
-import SignIn from "./pages/SignIn";
-import {Provider} from 'react-redux';
-import {store, persistor} from "./store";
-import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import SignIn from './pages/SignIn';
+import { store, persistor } from './store';
 
-console.log(`Print custom variable, must have REACT_APP_ preffix: ${process.env.REACT_APP_ADRIANO}`, process.env)
+console.log(
+  `Print custom variable, must have REACT_APP_ preffix: ${process.env.REACT_APP_ADRIANO}`,
+  process.env,
+);
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-              <SignIn />
-          </PersistGate>
-      </Provider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <SignIn />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
